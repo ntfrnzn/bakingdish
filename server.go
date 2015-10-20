219876f61dc6b991366a562a56fcc179383a356a
Nathan Franzen <nfranzen+github@gmail.com>
2015-10-20 07:25:04 -0700

    introduce middleware api substitution

--- server.go
+++ server.go
@@ -1,40 +1,82 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	//	"github.com/corneldamian/json-binding"
+	"reflect"
 	"encoding/json"
 	"github.com/gocraft/web"
 	"github.com/ntfrnzn/bakingdish/models"
 	"io"
 	"io/ioutil"
+	// "log"
 	"net/http"
-	"strings"
+        "fmt"
+	// "github.com/corneldamian/json-binding"
+	// "strings"
+        // "github.com/gocraft/health"
 )
 
+
+type Query struct {
+}
+
+type RecipeId struct {
+	Id string
+	Name string
+}
+
+type RecipeApi interface {
+	GetRecipe(id string) (*models.Recipe, error)
+	PostRecipe(recipe *models.Recipe) (string, error)
+        SearchRecipe(query *Query) ([]RecipeId, error)
+}
+
+
+type TestingApi struct {
+}
+
+
+func (TestingApi) GetRecipe(id string) (*models.Recipe, error) {
+	web.Logger.Println("get recipe for id: " + id)
+	return models.GetExample() 
+}
+
+func (TestingApi) SearchRecipe(query *Query) ([]RecipeId, error) {
+	web.Logger.Println("search recipes for id")
+	return []RecipeId{ {"dummy_id","Indian-Style Grilled Tuna Steaks with Aromatic Spice Paste"} }, nil
+}
+
+func (TestingApi) PostRecipe( recipe *models.Recipe) (string, error) {
+	content, _ := json.Marshal(recipe)
+	web.Logger.Println(string(content))
+	content, err := json.Marshal(recipe)
+	if (err != nil){
+		return "", err
+	}
+	web.Logger.Println( "posted the recipe: " + string(content))
+        return "dummy_id", nil
+}
+
+
+
 type Context struct {
-	HelloCount int
+   api RecipeApi
 }
 
-func (c *Context) SetHelloCount(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
-	c.HelloCount = 3
-	next(rw, req)
+
+func (c *Context) TestApiMiddleware(rw web.ResponseWriter, req *web.Request, next web.NextMiddlewareFunc) {
+        web.Logger.Println("here we are in middleware")
+	c.api = TestingApi{}
+	web.Logger.Println( reflect.TypeOf( c ).String() )
+	web.Logger.Println( reflect.TypeOf( (*c).api ).String() )
+        next(rw, req)
 }
 
-func (c *Context) SayHello(rw web.ResponseWriter, req *web.Request) {
-	fmt.Fprint(rw, strings.Repeat("Hello ", c.HelloCount), "World!")
-}
 
-func (c *Context) EchoParams(rw web.ResponseWriter, req *web.Request) {
-	fmt.Fprint(rw, req.PathParams["param1"])
 
-}
 
-func (c *Context) TestRecipe(rw web.ResponseWriter, req *web.Request) {
-	//r := models.Recipe{}
-	//r.Name="Toasted Cheese"
-	r, _ := models.GetExample()
+func (c *Context) GetRecipe(rw web.ResponseWriter, req *web.Request) {
+        id := req.PathParams["id"]
+        r, _ := (*c).api.GetRecipe(id)
 	rw.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(r)
 }
@@ -56,25 +98,60 @@ func (c *Context) PostRecipe(rw web.ResponseWriter, req *web.Request) {
 		}
 	}
 
-	rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
-	rw.WriteHeader(http.StatusCreated)
-	if err := json.NewEncoder(rw).Encode(recipe); err != nil {
+        id, err := (*c).api.PostRecipe(&recipe)
+        if (err != nil ){
 		panic(err)
 	}
-	content, _ := json.Marshal(recipe)
-	log.Println(string(content))
+        
+	rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	rw.WriteHeader(http.StatusCreated)
+	fmt.Fprint(rw, `{"id":"`+id+`"}"`)
 }
+
+
+func (c *Context) SearchRecipe(rw web.ResponseWriter, req *web.Request) {
+	var query Query
+	body, err := ioutil.ReadAll(io.LimitReader(req.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := req.Body.Close(); err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, &query); err != nil {
+		rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		rw.WriteHeader(422) // unprocessable entity
+		if err := json.NewEncoder(rw).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+
+        results, err := (*c).api.SearchRecipe(&query)
+        if (err != nil ){
+		panic(err)
+	}
+        
+	rw.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	json.NewEncoder(rw).Encode(results)
+}
+
+
 
 func main() {
 
-	router := web.New(Context{}). // Create your router
-					Middleware(web.LoggerMiddleware).     // Use some included middleware
-					Middleware(web.ShowErrorsMiddleware). // ...
-		// documentation for the below is not terribly clear -- in order to get the automatic loading of
-		// index.html, it _must_ be specified as a web.StaticOption
+	// regarding the static middleware (angular etc) documentation
+	// for the below is not terribly clear -- in order to get the
+	// automatic loading of index.html, it _must_ be specified as
+	// a web.StaticOption
+
+	// Create your router
+	router := web.New( Context{}  ).
+		Middleware(web.LoggerMiddleware).     // Use some included middleware
+		Middleware(web.ShowErrorsMiddleware). // ...
+		Middleware((*Context).TestApiMiddleware).   // use the Test version of the methods
 		Middleware(web.StaticMiddlewareFromDir(http.Dir("static"), web.StaticOption{Prefix: "", IndexFile: "index.html"})).
 		Post("/recipe", (*Context).PostRecipe).
-		Get("/recipe", (*Context).TestRecipe).
-		Get("/:param1", (*Context).EchoParams)
+		Get("/recipe/:id", (*Context).GetRecipe).
+		Post("/search/", (*Context).SearchRecipe)
 	http.ListenAndServe("localhost:3000", router) // Start the server!
 }
--- static/testing/recipe.js
+++ static/testing/recipe.js
@@ -1,7 +1,11 @@
-function Recipe($scope, $http) {
-    $http.get('/recipe').
-        success(function(data) {
-            $scope.recipe = data;
+
+
+var recipeApp = angular.module('recipeApp', []);
+recipeApp.controller('RecipeCtrl', function ($scope, $http){
+    $http.get('/recipe/dummy_id').success(function(data) {
+          $scope.recipe = data;
         });
-}
+});
+
+
 
